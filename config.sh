641bb13882281b24bdfb20c57188b4ebc540c1f8
Marcus Aurelius <marcus+git@aurelius.org>
2021-05-15 17:31:30 -0700

    Wrapped lexer and parser into functions
    Lexer and parser can now be called on sequential files, though I still
    need to reset the associated global variables at the beginning of each
    run.
    
    Beginning to work on deployment

--- config.sh
+++ config.sh
@@ -3,19 +3,31 @@
 
 # How to identify this computer. E.g., which config options to load? Should be a
 # descriptive 'class' that accepts a common config. E.g., "macos" or "ubuntu".
-# Or maybe "personal" vs. "work". Imacts which configuration variables are
+# Or maybe "personal" vs. "work". Impacts which configuration variables are
 # sourced from the config/files/$filetype/{options,additions}
 class=
 
 # Methodology by which to 'deploy' the dotfiles to their respective location.
-#  slink :: `ln -s` (symlink)
-#  hlink :: `ln` (hard link)
-#  copy  :: `cp`
-deploy_mode='link'
+#  slink :: `ln -sr` (symlink)
+#  hlink :: `ln -r`  (hard link)
+#  icopy :: `cp -i`  (interactive copy)
+#  copy  :: `cp`     (non-interactive copy)
+# Default is icopy'
+deploy_mode='icopy'
 
 # If there is an existing file at the $destination, should (and if so, how) the
 # existing file be backed up?
-#  dir  :: `mv` the file to config/backup/$filename (removing leading '.')
-#  bak  :: `mv` in place, with a '.bak' extension
-#  none :: No backup performed, file is overwritten if exists
+#  dir  :: `mv` existing file to backup/$filename (removing leading '.')
+#  bak  :: `mv` in place, append '.bak' suffix
+#  rm   :: Exiting file is removed, then replaced
+#  irm  :: Existing file is interactively removed, then replaced
+# Default is 'dir'
 backup_mode='dir'
+
+# How to handle missing keys? i.e., if class[key] is undefined
+#  warn  :: warn user errors were found, but leave text as is
+#  repl  :: warn & replace text with 'KEY_ERROR($key)'
+#  rm    :: warn & replace with empty string
+#  quiet :: do not warn user, leave text as is
+# Default is 'repl'
+missing_key='repl'
--- deploy-dotfiles.sh
+++ deploy-dotfiles.sh
@@ -3,8 +3,8 @@
 #  2021-04-30  :: Created
 #  2021-05-14  :: Improved token generation (now uses globally incrementing #,
 #                 raerpb len(tokens)--more robust) and improved parser.
-
-#───────────────────────────────────( notes )───────────────────────────────────
+#
+#───────────────────────────────────( todo )────────────────────────────────────
 # Should have a CLI option to add a new config file more easily. Will
 # automatically create the requisite directory structure, and (if specified),
 # `cp` a file in to initially serve as ./files/$name/base, raerpb the
@@ -12,9 +12,16 @@
 # then if so assume it's a path to the base file. Example:
 # ./deploy-dotfiles --new ~/.vimrc
 
+#═══════════════════════════════════╡ BEGIN ╞═══════════════════════════════════
 #──────────────────────────────────( prereqs )──────────────────────────────────
-_debug=false
+# TODO: Some of these features actually need bash 4.2, not just 4.
+[[ ${BASH_VERSINFO[0]} -lt 4 ]] && {
+   fname="$(basename ${BASH_SOURCE[0]})"
+   echo -e "\n[${fname}] ERROR: Requires Bash version >= 4\n"
+   exit 1
+}
 
+# TODO: This will be later imported, not explicitly defined. `import mk-colors`
 # Colors:
 rst=$(tput sgr0)                                   # Reset
 bk="$(tput setaf 0)"                               # Black
@@ -26,60 +33,87 @@ mg="$(tput setaf 5)"  ;  bmg="$(tput bold)${bl}"   # Magenta ;  Bright Magenta
 cy="$(tput setaf 6)"  ;  bcy="$(tput bold)${cy}"   # Cyan    ;  Bright Cyan
 wh="$(tput setaf 7)"  ;  bwh="$(tput bold)${wh}"   # White   ;  Bright White
 
-# Version requirement: >4
-[[ ${BASH_VERSION%%.*} -lt 4 ]] && {
-   fname="$(basename ${BASH_SOURCE[0]})"
-   echo -e "\n[${fname}] ERROR: Requires Bash version >= 4\n"
-   exit 1
-}
+#(
+#   printf '╒═════════════════════════════════════════════╕\n'
+#   printf '├──────────────────┤ START ├──────────────────┤\n'
+#   printf '│ %s - ' "$(date '+%Y/%b/%d %T')"
+#   printf '%-20s │\n' $(printf '%.20s' $(basename "${BASH_SOURCE[0]%.*}"))
+#   printf '╘═════════════════════════════════════════════╛\n'
+#)
 
-#═══════════════════════════════════╡ BEGIN ╞═══════════════════════════════════
+#──────────────────────────────────( global )───────────────────────────────────
 PROGDIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd )"
 
-# TODO: This is for testing, so everything can stay in the same directory. Will
-#       eventually move over to it's home of ~/.config/hre-utils/deploy-dotfiles
-#CONFDIR="${XDG_CONFIG_HOME:-~/.config}/hre-utils/deploy-dotfiles"
-CONFDIR="${PROGDIR}"
-CONFFILE="${CONFDIR}/config.sh"
-
-function debug {
-   local lineno=${BASH_LINENO[0]}
-   local fname=$(basename "${BASH_SOURCE[0]}")
-   local date=$(date '+%Y/%b/%d %T')
-
-   local text color
-   if [[ $# -gt 1 ]] ; then
-      case $1 in
-         debug|DEBUG)   color="${cy}" ;;
-         warn|WARN)     color="${yl}" ;;
-         crit|CRIT)     color="${brd}" ;;
-         *)             color="${wh}" ;;
-      esac
-      text="$2"
-   else
-      text="$1"
-   fi
-
-   $_debug && {
-      printf "[${fname%.*}] $date, ln.%04d\n"  $lineno
-      printf " ${color}└── ${text}${rst}\n"
-   }
-}
-
-#────────────────────────────────( validation )─────────────────────────────────
-#if [[ ! -e "${PROGDIR}/files/*/base" ]] ; then
-#   echo "Not all files in config/files/ have a 'base'."
-#   exit 1
-#fi
-
-#readarray -t -d $'\n' FILESDIR < <(ls -1 "${CONFDIR}/files")
+# Directory in which we're currently working, e.g., ./files/vimrc
+declare -g WORKING_DIR
 
 # Array for holding the names of the dynamically generated associative arrays
 # with token properties.
 declare -a TOKENS
 declare -i TOKEN_IDX=0
-declare LAST_CREATED_TOKEN
+declare -g LAST_CREATED_TOKEN
 
+# Error tracking:
+declare -a ERR_KEY_NOT_FOUND
+
+#───────────────────────────────( import config )───────────────────────────────
+declare -g CONFDIR="${PROGDIR}"
+declare -g GCONF="${CONFDIR}/config"
+# Global config file, in contrast to a locally loaded config file on a per-
+# directory basis, specified as `FCONF`.
+#
+# TODO: This is for testing, so everything can stay in the same directory. Will
+#       eventually move over to it's home of ~/.config/hre-utils/deploy-dotfiles
+#CONFDIR="${XDG_CONFIG_HOME:-~/.config}/hre-utils/deploy-dotfiles"
+#mkdir -p "$CONFDIR"
+
+#──────────────────────────────( tmp & debugging )──────────────────────────────
+_debug=false
+
+function debug {
+   $_debug || return 0
+
+   # Only display debug messages at or above the minimum:
+   [[ $1 -lt $__debug_min__ ]] && return 0
+
+   local loglvl lineno=${BASH_LINENO[0]}
+   local text color
+
+   case $1 in
+      0) color="${cy}"  ; loglvl='DEBUG' ;;
+      1) color="${wh}"  ; loglvl='INFO'  ;;
+      2) color="${yl}"  ; loglvl='WARN'  ;;
+      3) color="${brd}" ; loglvl='CRIT'  ;;
+      *) color="${wh}"  ; loglvl='INFO'  ;;
+   esac
+   text="$2"
+
+   printf '[%-5s] ln.%04d :: %s\n'  "$loglvl"  "$lineno"  "$text"
+}
+
+
+function debug_tokens {
+   for tname in "${TOKENS[@]}" ; do
+      declare -n tref=$tname
+      local col="${cy}"
+      [[ ${tref[type]} == 'OPEN'  ]] && col="${bl}"
+      [[ ${tref[type]} == 'CLOSE' ]] && col="${bl}"
+      [[ ${tref[type]} == 'KEY'   ]] && col="${bgr}"
+
+      # def __repr__(self):
+      printf "Token(${col}%-9s${rst} '${col}%s${rst}')\n" "${tref[type]}," "${tref[value]}"
+   done
+}
+
+
+function debug_output {
+   for _v in "${TOKENS[@]}" ; do
+      declare -n v="$_v"
+      printf -- "${v[value]}"
+   done
+}
+
+#═══════════════════════════════════╡ LEXER ╞═══════════════════════════════════
 function Token {
    # Token types:
    #  OPEN
@@ -106,171 +140,217 @@ function Token {
    t[type]="$type"
    t[value]="$value"
 
-   debug "Created Token(${t[type]}, ${t[value]})"
+   debug 0 "Created Token(${t[type]}, ${t[value]})"
 }
 
-
-function debug_tokens {
-   for tname in "${TOKENS[@]}" ; do
-      declare -n tref=$tname
-      local col="${cy}"
-      [[ ${tref[type]} == 'OPEN'  ]] && col="${bl}"
-      [[ ${tref[type]} == 'CLOSE' ]] && col="${bl}"
-      [[ ${tref[type]} == 'KEY'   ]] && col="${bgr}"
-
-      printf "Token(${col}%-9s${rst} '${col}%s${rst}')\n" "${tref[type]}," "${tref[value]}"
-   done
-}
-
-#═══════════════════════════════════╡ LEXER ╞═══════════════════════════════════
 
 # Scan forwards, filling found characters into a buffer.
 function fill {
    delim=$1
    declare -g buffer=''
 
-   while [[ $idx -lt $len ]] ; do
+   while [[ $idx -lt ${#chararray[@]} ]] ; do
       local c="${chararray[$idx]}"
       local n="${chararray[$((idx+1))]}"
 
       buffer+="$c"
-      [[ "$n" == [{}#] ]] && break
+      [[ "$n" =~ [{}] ]] && break
 
       idx=$((idx+1))
    done
 
-   debug "Buffer filled: [$buffer]"
+   debug 0 "Buffer filled: [$buffer]"
 }
 
 
-# Load characters into array, so we may iterate over them (and look
-# forwards/backwards more easily):
-declare -a chararray
-while read -r -N1 c ; do
-   chararray+=( "$c" )
-done < "${CONFDIR}/files/vimrc/base"
+function lex {
+   # Initially load characters into array, so we may iterate over them (and look
+   # forwards/backwards more easily):
+   declare -a chararray
+   while read -r -N1 c ; do
+      chararray+=( "$c" )
+   done < "${WORKING_DIR}/base"
 
-len=${#chararray[@]}
-declare -i idx=0
+   # Iterate over chararray, create tokens of either 'OPEN', 'CLOSE', or 'TEXT'
+   declare -i idx=0
+   while [[ $idx -lt ${#chararray[@]} ]] ; do
+      c="${chararray[$idx]}"
 
-while [[ $idx -lt $len ]] ; do
-   c="${chararray[$idx]}"
-
-   # Capture comments
-   if [[ "$c" == "#" ]] ; then
-      fill ; Token 'COMMENT' "$buffer"
-
-   # Start block (or potentially a regular character)
-   elif [[ "$c" == '{' ]] ; then
-      Token 'OPEN' "$c"
-
-   # End block
-   elif [[ "$c" == '}' ]] ; then
-      Token 'CLOSE' "$c"
-
-   # The rest of the characters
-   else
-      # If there are tokens in the stack, get the type of the last token
-      # appended to the stack. Must first check if it exists, or subscripting
-      # 'tokens' will fail:
-      if [[ ${#TOKENS[@]} -gt 0 ]] ; then
-         declare -n last_token="${TOKENS[-1]}"
-         last_type="${last_token[type]}"
-      fi
-
-      if [[ $last_type == 'OPEN' ]] ; then
+      if [[ "$c" == '{' ]] ; then
+         Token 'OPEN' "$c"
+      elif [[ "$c" == '}' ]] ; then
+         Token 'CLOSE' "$c"
+      else
          fill ; Token 'TEXT' "$buffer"
       fi
-   fi
 
-   idx=$(( idx+1 ))
-done
-
+      idx=$(( idx+1 ))
+   done
+}
 
 #══════════════════════════════════╡ PARSER ╞═══════════════════════════════════
 #─────────────────────────────────( fill keys )─────────────────────────────────
-
-# New 'buffer' array to hold the tokens as we iterate over them.
-unset buffer ; declare -a buffer
-declare -i level=0                  # <- OPEN 'indentation' level
-declare -i idx=0                    # <- Current index of tokens[]
-
 function is_a_key {
+   [[ ! $level -ge 2 ]] && return 1
+
    # Must be of type "text"
-   [[ ! ${token[type]} == 'TEXT' ]] && return 1
+   [[ ! ${token[type]} == 'TEXT' ]] && return 2
 
-   # Must have >=2 tokens left to process, and the current 'OPEN level' must be
-   # >=2, else we're not in a {{...}} block:
-   [[ ! ${#TOKENS[@]} -ge 2      ]] && return 1
-   [[ ! ${level}      -ge 2      ]] && return 1
+   declare -n before_2=${TOKENS[$((idx-1))]}    # <-.
+   declare -n before_1=${TOKENS[$((idx-2))]}    # <-+- Two preceding tokens
 
-   declare -n b_2=${buffer[-1]} b_1=${buffer[-2]}
-   declare -n t_1=${TOKENS[$((idx+1))]}  t_2=${TOKENS[$((idx+2))]}
+   declare -n after_1=${TOKENS[$((idx+1))]}     # <-.
+   declare -n after_2=${TOKENS[$((idx+2))]}     # <-+- Two subsequent tokens
 
    # Must start with 2x '{'...
-   [[ ! ${b_2[type]}  == 'OPEN'  ]] && return 1
-   [[ ! ${b_1[type]}  == 'OPEN'  ]] && return 1
+   [[ ${before_2[type]} != 'OPEN' || ${before_1[type]} != 'OPEN' ]] && {
+      debug 0 "$token_name, prior 2 not '{'"
+      return 3
+   }
 
-   # ...and end with  2x '}'
-   [[ ! ${t_1[type]}  == 'CLOSE' ]] && return 1
-   [[ ! ${t_2[type]}  == 'CLOSE' ]] && return 1
+   # ...and end with 2x '}'
+   [[ ${after_1[type]} != 'CLOSE' || ${after_2[type]} != 'CLOSE' ]] && {
+      debug 0 "$token_name, next 2 not '}'"
+      return 4
+   }
 
    return 0
 }
 
 
 function munch {
-   # Pop last two from buffer[]
-   buffer=( "${buffer[@]::$((${#buffer[@]}-2))}" )
-
    # Slice from 0 -> 2 before the current idx. Should be non-inclusive of both
    # the opening '{{' characters:
    local lower=( "${TOKENS[@]::$((idx-2))}" )
 
-   # Slice from 2 after current idx to end of array. Cuts out the closing '}}':
-   local upper=( "${TOKENS[@]:$((idx+2)):$((${#TOKENS[@]}-idx))}" )
+   # Slice from 3 after current idx to end of array. Starts on the character
+   # *after* the closing '}}':
+   local upper=( "${TOKENS[@]:$((idx+3)):$((${#TOKENS[@]}-idx))}" )
 
-   # Create new token based on the dictionary lookup:
-   Token 'TEXT' '-----------'
+   local value="$(get_value "${token[value]}")"
+   if [[ -n $value ]] ; then
+      Token 'TEXT' "$value"
+   else
+      # TODO: `case $missing_key in`
+      Token 'TEXT' "KEY_ERROR(${token[value]})"
+   fi
 
-   # Add new value to TOKENS stack & buffer
    TOKENS=( "${lower[@]}"  "$LAST_CREATED_TOKEN"  "${upper[@]}" )
-   buffer+=( "$LAST_CREATED_TOKEN" )
+   ((level-2))
 }
 
 
-while [[ $idx -lt ${#TOKENS[@]} ]] ; do
-   token_name="${TOKENS[$idx]}"
-   declare -n token="${token_name}"
+function get_value {
+   declare -A pretend_dict=(
+      [shiftwidth]='SHIFTWIDTH'
+      [tabstop]='TABSTOP'
+      [triple]='TRIPLE'
+      #[this]='THIS'
+      [eol]='<END>'
+   )
 
-   # Track current indentation level.
-   [[ ${token[type]} == 'OPEN'  ]] && ((level++))
-   [[ ${token[type]} == 'CLOSE' ]] && ((level--))
-
-   if is_a_key ; then
-      munch
-   else
-      buffer+=( $token_name )
-   fi
-
-   ((idx++))
-done
-
+   echo "${pretend_dict[$1]}"
+}
 
 #──────────────────────────────( strip comments )───────────────────────────────
-#──────────────────────────────( strip newlines )───────────────────────────────
+function strip_comments {
+   echo pass
+}
 
-for _v in "${buffer[@]}" ; do
-   declare -n v="$_v"
-   printf -- "${v[value]}"
+#──────────────────────────────( strip newlines )───────────────────────────────
+function strip_newlines {
+   echo pass
+}
+
+#───────────────────────────────────( parse )───────────────────────────────────
+function parse {
+   declare -gi level=0 idx=0
+
+   # Tokens may only occur if concluded by: '}', '}'. Stop scanning if there are
+   # not at least two subsequent tokens to read.
+   while [[ $idx -lt $(( ${#TOKENS[@]} - 1 )) ]] ; do
+      token_name="${TOKENS[$idx]}"
+      declare -n token="${token_name}"
+
+      # Track current OPEN "indentation" level:
+      [[ ${token[type]} == 'OPEN'  ]] && ((level++))
+      [[ ${token[type]} == 'CLOSE' ]] && ((level--))
+
+      is_a_key && munch
+
+      ((idx++))
+   done
+
+   # [[ $strip_comments =~ ([Tt]rue|[Yy]es) ]] && strip_comments
+   # [[ $strip_newlines =~ ([Tt]rue|[Yy]es) ]] && strip_newlines
+}
+
+
+#══════════════════════════════════╡ DEPLOY ╞═══════════════════════════════════
+# This is where the actually deployment stuff will go. Largely the same thing as
+# the `debug_output` function, but pipes to the destination file after
+# potentially backing up the existing file.
+#──────────────────────────────────( backup )───────────────────────────────────
+function backup_existing {
+   local cmd
+
+   case $backup_mode in
+      bak)  # In-place backup, re-naming existing file to ${file}.bak
+            mv "$dest" "${dest}.bak" ;;
+
+      rm)   # Nukes existing file--AAAH!
+            rm -f "$dest" ;;
+
+      irm)  # Interactively nukes existing file--aaah!
+            rm -i "$dest" ;;
+
+      *)    # Default option, moves existing file to ./backup/ directory, mildly
+            # assuring a unique name:
+            local fname="$(basename "${dest}").$(date '+%s')"
+            mv "$dest" "${CONFDIR}/backup/${fname}" ;;
+   esac
+}
+
+
+#───────────────────────────────────( write )───────────────────────────────────
+function deploy {
+
+
+   case
+      slink) cmd='ln -sr' ;;
+      hlink) cmd='ln -r'  ;;
+      copy)  cmd='cp'     ;;
+      icopy) cmd='cp -i'  ;;
+      *)     cmd='cp -i'  ;;
+   esac
+   debug 1 "Deploy method: \`$cmd\`"
+}
+
+#═══════════════════════════════════╡ MAIN ╞════════════════════════════════════
+#─────────────────────────────────( argparse )──────────────────────────────────
+while [[ $# -gt 0 ]] ; do
+   case $1 in
+      --debug)
+            shift
+            _debug=true
+            __debug_min__=$1
+            ;;
+
+      *)
+            __invalid__+=( $1 )
+            shift
+            ;;
+   esac
 done
 
-echo
+#──────────────────────────────────( iterdir )──────────────────────────────────
+for wdir in $(ls "${CONFDIR}/files") ; do
+   WORKING_DIR="${CONFDIR}/files/$wdir"
 
-# TODO;CURRENT;
-# Do we actually need the intermediate buffer? Now that we've changed how tokens
-# are generated, and they're not based on the length of the TOKENS array, we
-# should be able to just modify in-place. Should drastically simplify the checks
-# we need to perform as well.
-#
-# Need to also add a `((level-2))` to the end of `munch()`
+   if [[ ! -e "${WORKING_DIR}/base" ]] ; then
+      debug 2 "No \`base\` file found in ${WORKING_DIR}"
+      continue
+   fi
+
+   lex ; parse ; debug_output
+done
